bc7154bbdabd7ee46af727923a15ffff2f72e146
MARTA <marta.alarcon.7e7@itb.cat>
2024-01-06 16:55:16 +0100

    funcion cuadrado closes #1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,37 @@ namespace Activitat
     {
         public static void Main()
         {
-
+            const string MsgChoose = "1 - área cuadrado, 2 - área rectángulo, 3 - área triángulo";
+            const string MsgNumberOne = "Introduce el primer dato";
+            const string MsgNumberTwo = "Introduce el segundo dato";
+            const int two = 2;
+            int numberOne, numberTwo, select;
+            Console.WriteLine(MsgChoose);
+            select = Convert.ToInt32(Console.ReadLine());
+            switch (select)
+            {
+                case 1:
+                    Console.WriteLine(MsgNumberOne);
+                    numberOne = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(MsgNumberTwo);
+                    numberTwo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(CalcularRectanguloCuadrado(numberOne, numberTwo));
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
         }
+        static int CalcularRectanguloCuadrado (int numberOne, int numberTwo) 
+        {
+            int result;
+            result = numberOne * numberTwo;
+            return result;
+        }
+
+
+
+
     }
 }
